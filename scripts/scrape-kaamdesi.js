d487254c161b231992f85e8e3cbf2d3a93537e24
swift200923 <amrita232324@gmail.com>
2025-12-22 19:41:10 +0530

    Update scrape-kaamdesi.js

--- scripts/scrape-kaamdesi.js
+++ scripts/scrape-kaamdesi.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import fetch from "node-fetch";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 
 const BASE = "https://kaamdesi.com";
 const LISTING_PAGES = [
